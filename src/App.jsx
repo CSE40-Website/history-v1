c8c0765b2efd5c1daf4dba0a76dd208b2a27c394
Lahiru-Randika <lahirurandika600@gmail.com>
2024-10-16 20:05:48 +0530

    Update App.jsx and add routes | add /history route

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Loader } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import { Experience } from "./components/Experience";
-import { UI } from "./components/UI";
+import { Experience } from "./components/History/Experience";
+import { UI } from "./components/History/UI";
 
 function App() {
   return (
-    <>
-      <UI />
-      <Loader />
-      <Canvas shadows camera={{ position: [-0.5, 1, 4], fov: 45 }}>
-        <group position-y={0}>
-          <Suspense fallback={null}>
-            <Experience />
-          </Suspense>
-        </group>
-      </Canvas>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/history" exact={true}
+          element={<>
+            <UI />
+            <Loader />
+            <Canvas shadows camera={{ position: [-0.5, 1, 4], fov: 45 }}>
+              <group position-y={0}>
+                <Suspense fallback={null}>
+                  <Experience />
+                </Suspense>
+              </group>
+            </Canvas>
+          </>} 
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
